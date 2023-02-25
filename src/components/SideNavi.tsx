b1c114a8127f1bacf17817e8454645472519c516
Kutlu Karakoc <karakockutlu@gmail.com>
2023-02-26 02:44:18 +0300

    added side navi structure

--- src/components/SideNavi.tsx
+++ src/components/SideNavi.tsx
@@ -1,9 +1,27 @@
 import '../styles/sidenavi.scss'
+import { AiFillHome } from 'react-icons/ai'
 
 const SideNavi = () => {
-    return (
-        <div className="sidenavi">SideNavi</div>
-    )
+   return (
+      <div className="sidenavi">
+
+         <div className='navi__section'>
+            <div className='navi__section-entry'>
+               <div><AiFillHome size='1.8em'/></div>
+               <div className='navi__entry-title'>Home</div>
+            </div>
+            <div className='navi__section-entry'>
+               <div><AiFillHome size='1.8em'/></div>
+               <div className='navi__entry-title'>Home</div>
+            </div>
+            <div className='navi__section-entry'>
+               <div><AiFillHome size='1.8em'/></div>
+               <div className='navi__entry-title'>Home</div>
+            </div>
+         </div>
+
+      </div>
+   )
 }
 
 export default SideNavi
---
+++ src/constants/sideNavi.ts
@@ -0,0 +1,34 @@
+export const naviItems = [
+   [
+      {
+         'icon': 'AiFillHome',
+         'title': 'Home'
+      },
+      {
+         'icon': 'AiFillHome',
+         'title': 'Shorts'
+      },
+      {
+         'icon': 'AiFillHome',
+         'title': 'Subscriptions'
+      }
+   ],
+   [
+      {
+         'icon': 'AiFillHome',
+         'title': 'Library'
+      },
+      {
+         'icon': 'AiFillHome',
+         'title': 'History'
+      },
+      {
+         'icon': 'AiFillHome',
+         'title': 'Watch Later'
+      },
+      {
+         'icon': 'AiFillHome',
+         'title': 'Liked videos'
+      }
+   ]
+]
