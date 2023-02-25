89e764d0e064140a64242c748a6ecef8c627db6f
Kutlu Karakoc <karakockutlu@gmail.com>
2023-02-25 23:49:28 +0300

    added main routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import './styles/app.scss'
-import Home from "./pages/Home"
+import Home from './pages/Home'
+import SearchResults from './pages/SearchResults'
+import Watch from './pages/Watch'
+import SearchBar from './components/SearchBar'
+import { Routes, Route } from 'react-router-dom'
 
 const App: React.FC = () => {
    return (
       <div>
-         <Home />
+         <SearchBar />
+         <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/searchresults" element={<SearchResults />} />
+            <Route path="/watch" element={<Watch />} />
+         </Routes>
       </div>
    )
 }
