d947bdba7148b63f59f61a8309d74b2cab7bd868
Kutlu Karakoc <karakockutlu@gmail.com>
2023-02-26 18:42:10 +0300

    added structure of home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,289 @@ import '../styles/home.scss'
 
 const Home: React.FC = () => {
    return (
-      <div className='home'>Home</div>
+      <div className='home'>
+         <div className="home__content-wrapper">
+            <div className='home__content-player'></div>
+            <div className='home__content-details'>
+               <div className='details__avatar'>
+                  <img src="https://yt3.ggpht.com/1qqfKVq2KK6PurSulC3OPAOXeH7jk4Ze9d80BB3-OqRy_HXxyUrFmDNYvZKyKDb8tJy2rliBWQ=s68-c-k-c0x00ffffff-no-rj" alt="" />
+               </div>
+               <div className='details__meta'>
+                  <h3 className="details__title">ZENGİN İNSANLARIN KAHVALTILARINI DENEDİK!</h3>
+                  <div className="details__post-info">
+                     <div className='details__channel-name'>TepkiKolik</div>
+                     <div className='details__metadata'>
+                        <div className='details__metadata-views'>294K views</div>
+                        <div className='details__metadata-divider'>.</div>
+                        <div className='details__metadata-date'>1 day ago</div>
+                     </div>
+                  </div>
+               </div>
+               <div className='details__menu'></div>
+            </div>
+         </div>
+
+         <div className="home__content-wrapper">
+            <div className='home__content-player'></div>
+            <div className='home__content-details'>
+               <div className='details__avatar'>
+                  <img src="https://yt3.ggpht.com/1qqfKVq2KK6PurSulC3OPAOXeH7jk4Ze9d80BB3-OqRy_HXxyUrFmDNYvZKyKDb8tJy2rliBWQ=s68-c-k-c0x00ffffff-no-rj" alt="" />
+               </div>
+               <div className='details__meta'>
+                  <h3 className="details__title">ZENGİN İNSANLARIN KAHVALTILARINI DENEDİK!</h3>
+                  <div className="details__post-info">
+                     <div className='details__channel-name'>TepkiKolik</div>
+                     <div className='details__metadata'>
+                        <div className='details__metadata-views'>294K views</div>
+                        <div className='details__metadata-divider'>.</div>
+                        <div className='details__metadata-date'>1 day ago</div>
+                     </div>
+                  </div>
+               </div>
+               <div className='details__menu'></div>
+            </div>
+         </div>
+         <div className="home__content-wrapper">
+            <div className='home__content-player'></div>
+            <div className='home__content-details'>
+               <div className='details__avatar'>
+                  <img src="https://yt3.ggpht.com/1qqfKVq2KK6PurSulC3OPAOXeH7jk4Ze9d80BB3-OqRy_HXxyUrFmDNYvZKyKDb8tJy2rliBWQ=s68-c-k-c0x00ffffff-no-rj" alt="" />
+               </div>
+               <div className='details__meta'>
+                  <h3 className="details__title">ZENGİN İNSANLARIN KAHVALTILARINI DENEDİK!</h3>
+                  <div className="details__post-info">
+                     <div className='details__channel-name'>TepkiKolik</div>
+                     <div className='details__metadata'>
+                        <div className='details__metadata-views'>294K views</div>
+                        <div className='details__metadata-divider'>.</div>
+                        <div className='details__metadata-date'>1 day ago</div>
+                     </div>
+                  </div>
+               </div>
+               <div className='details__menu'></div>
+            </div>
+         </div>
+         <div className="home__content-wrapper">
+            <div className='home__content-player'></div>
+            <div className='home__content-details'>
+               <div className='details__avatar'>
+                  <img src="https://yt3.ggpht.com/1qqfKVq2KK6PurSulC3OPAOXeH7jk4Ze9d80BB3-OqRy_HXxyUrFmDNYvZKyKDb8tJy2rliBWQ=s68-c-k-c0x00ffffff-no-rj" alt="" />
+               </div>
+               <div className='details__meta'>
+                  <h3 className="details__title">ZENGİN İNSANLARIN KAHVALTILARINI DENEDİK!</h3>
+                  <div className="details__post-info">
+                     <div className='details__channel-name'>TepkiKolik</div>
+                     <div className='details__metadata'>
+                        <div className='details__metadata-views'>294K views</div>
+                        <div className='details__metadata-divider'>.</div>
+                        <div className='details__metadata-date'>1 day ago</div>
+                     </div>
+                  </div>
+               </div>
+               <div className='details__menu'></div>
+            </div>
+         </div>
+         <div className="home__content-wrapper">
+            <div className='home__content-player'></div>
+            <div className='home__content-details'>
+               <div className='details__avatar'>
+                  <img src="https://yt3.ggpht.com/1qqfKVq2KK6PurSulC3OPAOXeH7jk4Ze9d80BB3-OqRy_HXxyUrFmDNYvZKyKDb8tJy2rliBWQ=s68-c-k-c0x00ffffff-no-rj" alt="" />
+               </div>
+               <div className='details__meta'>
+                  <h3 className="details__title">ZENGİN İNSANLARIN KAHVALTILARINI DENEDİK!</h3>
+                  <div className="details__post-info">
+                     <div className='details__channel-name'>TepkiKolik</div>
+                     <div className='details__metadata'>
+                        <div className='details__metadata-views'>294K views</div>
+                        <div className='details__metadata-divider'>.</div>
+                        <div className='details__metadata-date'>1 day ago</div>
+                     </div>
+                  </div>
+               </div>
+               <div className='details__menu'></div>
+            </div>
+         </div>
+         <div className="home__content-wrapper">
+            <div className='home__content-player'></div>
+            <div className='home__content-details'>
+               <div className='details__avatar'>
+                  <img src="https://yt3.ggpht.com/1qqfKVq2KK6PurSulC3OPAOXeH7jk4Ze9d80BB3-OqRy_HXxyUrFmDNYvZKyKDb8tJy2rliBWQ=s68-c-k-c0x00ffffff-no-rj" alt="" />
+               </div>
+               <div className='details__meta'>
+                  <h3 className="details__title">ZENGİN İNSANLARIN KAHVALTILARINI DENEDİK!</h3>
+                  <div className="details__post-info">
+                     <div className='details__channel-name'>TepkiKolik</div>
+                     <div className='details__metadata'>
+                        <div className='details__metadata-views'>294K views</div>
+                        <div className='details__metadata-divider'>.</div>
+                        <div className='details__metadata-date'>1 day ago</div>
+                     </div>
+                  </div>
+               </div>
+               <div className='details__menu'></div>
+            </div>
+         </div>
+         <div className="home__content-wrapper">
+            <div className='home__content-player'></div>
+            <div className='home__content-details'>
+               <div className='details__avatar'>
+                  <img src="https://yt3.ggpht.com/1qqfKVq2KK6PurSulC3OPAOXeH7jk4Ze9d80BB3-OqRy_HXxyUrFmDNYvZKyKDb8tJy2rliBWQ=s68-c-k-c0x00ffffff-no-rj" alt="" />
+               </div>
+               <div className='details__meta'>
+                  <h3 className="details__title">ZENGİN İNSANLARIN KAHVALTILARINI DENEDİK!</h3>
+                  <div className="details__post-info">
+                     <div className='details__channel-name'>TepkiKolik</div>
+                     <div className='details__metadata'>
+                        <div className='details__metadata-views'>294K views</div>
+                        <div className='details__metadata-divider'>.</div>
+                        <div className='details__metadata-date'>1 day ago</div>
+                     </div>
+                  </div>
+               </div>
+               <div className='details__menu'></div>
+            </div>
+         </div>
+         <div className="home__content-wrapper">
+            <div className='home__content-player'></div>
+            <div className='home__content-details'>
+               <div className='details__avatar'>
+                  <img src="https://yt3.ggpht.com/1qqfKVq2KK6PurSulC3OPAOXeH7jk4Ze9d80BB3-OqRy_HXxyUrFmDNYvZKyKDb8tJy2rliBWQ=s68-c-k-c0x00ffffff-no-rj" alt="" />
+               </div>
+               <div className='details__meta'>
+                  <h3 className="details__title">ZENGİN İNSANLARIN KAHVALTILARINI DENEDİK!</h3>
+                  <div className="details__post-info">
+                     <div className='details__channel-name'>TepkiKolik</div>
+                     <div className='details__metadata'>
+                        <div className='details__metadata-views'>294K views</div>
+                        <div className='details__metadata-divider'>.</div>
+                        <div className='details__metadata-date'>1 day ago</div>
+                     </div>
+                  </div>
+               </div>
+               <div className='details__menu'></div>
+            </div>
+         </div>
+         <div className="home__content-wrapper">
+            <div className='home__content-player'></div>
+            <div className='home__content-details'>
+               <div className='details__avatar'>
+                  <img src="https://yt3.ggpht.com/1qqfKVq2KK6PurSulC3OPAOXeH7jk4Ze9d80BB3-OqRy_HXxyUrFmDNYvZKyKDb8tJy2rliBWQ=s68-c-k-c0x00ffffff-no-rj" alt="" />
+               </div>
+               <div className='details__meta'>
+                  <h3 className="details__title">ZENGİN İNSANLARIN KAHVALTILARINI DENEDİK!</h3>
+                  <div className="details__post-info">
+                     <div className='details__channel-name'>TepkiKolik</div>
+                     <div className='details__metadata'>
+                        <div className='details__metadata-views'>294K views</div>
+                        <div className='details__metadata-divider'>.</div>
+                        <div className='details__metadata-date'>1 day ago</div>
+                     </div>
+                  </div>
+               </div>
+               <div className='details__menu'></div>
+            </div>
+         </div>
+         <div className="home__content-wrapper">
+            <div className='home__content-player'></div>
+            <div className='home__content-details'>
+               <div className='details__avatar'>
+                  <img src="https://yt3.ggpht.com/1qqfKVq2KK6PurSulC3OPAOXeH7jk4Ze9d80BB3-OqRy_HXxyUrFmDNYvZKyKDb8tJy2rliBWQ=s68-c-k-c0x00ffffff-no-rj" alt="" />
+               </div>
+               <div className='details__meta'>
+                  <h3 className="details__title">ZENGİN İNSANLARIN KAHVALTILARINI DENEDİK!</h3>
+                  <div className="details__post-info">
+                     <div className='details__channel-name'>TepkiKolik</div>
+                     <div className='details__metadata'>
+                        <div className='details__metadata-views'>294K views</div>
+                        <div className='details__metadata-divider'>.</div>
+                        <div className='details__metadata-date'>1 day ago</div>
+                     </div>
+                  </div>
+               </div>
+               <div className='details__menu'></div>
+            </div>
+         </div>
+         <div className="home__content-wrapper">
+            <div className='home__content-player'></div>
+            <div className='home__content-details'>
+               <div className='details__avatar'>
+                  <img src="https://yt3.ggpht.com/1qqfKVq2KK6PurSulC3OPAOXeH7jk4Ze9d80BB3-OqRy_HXxyUrFmDNYvZKyKDb8tJy2rliBWQ=s68-c-k-c0x00ffffff-no-rj" alt="" />
+               </div>
+               <div className='details__meta'>
+                  <h3 className="details__title">ZENGİN İNSANLARIN KAHVALTILARINI DENEDİK!</h3>
+                  <div className="details__post-info">
+                     <div className='details__channel-name'>TepkiKolik</div>
+                     <div className='details__metadata'>
+                        <div className='details__metadata-views'>294K views</div>
+                        <div className='details__metadata-divider'>.</div>
+                        <div className='details__metadata-date'>1 day ago</div>
+                     </div>
+                  </div>
+               </div>
+               <div className='details__menu'></div>
+            </div>
+         </div>
+         <div className="home__content-wrapper">
+            <div className='home__content-player'></div>
+            <div className='home__content-details'>
+               <div className='details__avatar'>
+                  <img src="https://yt3.ggpht.com/1qqfKVq2KK6PurSulC3OPAOXeH7jk4Ze9d80BB3-OqRy_HXxyUrFmDNYvZKyKDb8tJy2rliBWQ=s68-c-k-c0x00ffffff-no-rj" alt="" />
+               </div>
+               <div className='details__meta'>
+                  <h3 className="details__title">ZENGİN İNSANLARIN KAHVALTILARINI DENEDİK!</h3>
+                  <div className="details__post-info">
+                     <div className='details__channel-name'>TepkiKolik</div>
+                     <div className='details__metadata'>
+                        <div className='details__metadata-views'>294K views</div>
+                        <div className='details__metadata-divider'>.</div>
+                        <div className='details__metadata-date'>1 day ago</div>
+                     </div>
+                  </div>
+               </div>
+               <div className='details__menu'></div>
+            </div>
+         </div>
+         <div className="home__content-wrapper">
+            <div className='home__content-player'></div>
+            <div className='home__content-details'>
+               <div className='details__avatar'>
+                  <img src="https://yt3.ggpht.com/1qqfKVq2KK6PurSulC3OPAOXeH7jk4Ze9d80BB3-OqRy_HXxyUrFmDNYvZKyKDb8tJy2rliBWQ=s68-c-k-c0x00ffffff-no-rj" alt="" />
+               </div>
+               <div className='details__meta'>
+                  <h3 className="details__title">ZENGİN İNSANLARIN KAHVALTILARINI DENEDİK!</h3>
+                  <div className="details__post-info">
+                     <div className='details__channel-name'>TepkiKolik</div>
+                     <div className='details__metadata'>
+                        <div className='details__metadata-views'>294K views</div>
+                        <div className='details__metadata-divider'>.</div>
+                        <div className='details__metadata-date'>1 day ago</div>
+                     </div>
+                  </div>
+               </div>
+               <div className='details__menu'></div>
+            </div>
+         </div>
+         <div className="home__content-wrapper">
+            <div className='home__content-player'></div>
+            <div className='home__content-details'>
+               <div className='details__avatar'>
+                  <img src="https://yt3.ggpht.com/1qqfKVq2KK6PurSulC3OPAOXeH7jk4Ze9d80BB3-OqRy_HXxyUrFmDNYvZKyKDb8tJy2rliBWQ=s68-c-k-c0x00ffffff-no-rj" alt="" />
+               </div>
+               <div className='details__meta'>
+                  <h3 className="details__title">ZENGİN İNSANLARIN KAHVALTILARINI DENEDİK!</h3>
+                  <div className="details__post-info">
+                     <div className='details__channel-name'>TepkiKolik</div>
+                     <div className='details__metadata'>
+                        <div className='details__metadata-views'>294K views</div>
+                        <div className='details__metadata-divider'>.</div>
+                        <div className='details__metadata-date'>1 day ago</div>
+                     </div>
+                  </div>
+               </div>
+               <div className='details__menu'></div>
+            </div>
+         </div>
+      </div>
    )
 }
 
