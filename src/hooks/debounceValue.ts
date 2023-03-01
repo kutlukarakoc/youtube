c2fa4d5dbb1f15ae23d1f40bed4d9c47078788e6
Kutlu Karakoc <karakockutlu@gmail.com>
2023-03-01 22:46:50 +0300

    added debounce value custom hook

---
+++ src/hooks/debounceValue.ts
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react'
+
+/* This custom hook takes in a value and a delay as parameters, and returns a debounced value */
+
+export const useDebouncedValue = (value: string, delay: number): string => {
+
+   // use state hook to store the debounced value
+   const [debouncedValue, setDebouncedValue] = useState(value)
+
+   // use effect hook to update the debounced value with a delay
+   useEffect(() => {
+      // timeout to update the debounced value after the specified delay
+      const debounceHandler = setTimeout(() => {
+         setDebouncedValue(value)
+      }, delay)
+
+      // clean up for unmount
+      return () => clearTimeout(debounceHandler)
+   }, [value, delay])
+
+   return debouncedValue
+}
